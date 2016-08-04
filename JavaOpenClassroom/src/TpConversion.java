8f902f440073fd63fa20432a96464337f3aa53dd
preston.siger <preston.siger@MGPW81VDI031.mgp.net>
2016-08-04 14:39:15 +0200

    avancement tp

--- JavaOpenClassroom/src/TpConversion.java
+++ JavaOpenClassroom/src/TpConversion.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
 
 public class TpConversion {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		//Variable
+		double celToFar;
+		double farToCel;
+		
+		System.out.println("CONVERTISSEUR DEGRES CELCIUS ET DEGRES FARENHEIT");
+		System.out.println("------------------------------------------------");
+		
+		System.out.println("Choisissez le mode de conversion : ");
+		System.out.println("1 - Converisseur Celcius -> Farenheit");
+		System.out.println("2 - Converisseur Farenheit -> Celcius");
+		
+		Scanner sc = new Scanner(System.in);
+		int choix = sc.nextInt();
+		
+		if (choix == 1)
+
+			//instruction
+		else if (choix == 2)
+			// instruction
+		else
+			//instrucion
 
 	}
 
